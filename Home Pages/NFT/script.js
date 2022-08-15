926aad2d01fb832b98e00bbfe347044d349a8cd2
abhinna1 <manandharabhinna@gmail.com>
2022-08-15 09:20:13 +0545

    init

---
+++ Home Pages/NFT/script.js
@@ -0,0 +1,24 @@
+console.log(document.querySelector('#nav-links'))
+document.getElementById('nav-links').style.left = '100%';
+document.getElementById('nav-toggler').addEventListener('click', ()=>{
+    let box = document.getElementById('nav-links');
+    if (box.style.left === '100%'){
+        box.style.left = 0;
+    }
+    else{
+        box.style.left = '100%';
+    }
+})
+
+document.addEventListener('scroll', ()=>{
+    let nav = document.querySelector('#navbar');
+    if(window.scrollY === 0){
+        nav.style.backgroundColor = 'transparent';
+        nav.style.borderBottom = 'none';
+    }
+    else{
+        nav.style.backgroundColor = 'white';
+        
+        nav.style.borderBottom = 'solid 2px';
+    }
+})
